53691c1b18a49cbaef20fb2a361230501e0cbe1a
Magdalena Grodzińska <mg370911@students.mimuw.edu.pl>
2017-05-12 16:35:32 +0200

    Added class files

---
+++ przyklad_liniowy.py
@@ -0,0 +1,58 @@
+from matplotlib import pyplot as plt
+from matplotlib import rc
+from nltk import FreqDist
+
+# ustawiamy czcionkę wspierającą unicode - w domyślnej są tylko znaki ASCII
+rc('font', family='DejaVu Sans')
+
+words = ['Wokulski', 'Rzecki', 'subiekt', 'handel', 'sklep']
+title1 = 'lalka-tom-pierwszy.txt'
+title2 = 'lalka-tom-drugi.txt'
+
+def line_plot(data1, data2, names):
+    plt.title("Liczba wystąpień słów")
+    plt.xlabel("Próbki")
+    plt.ylabel("Liczba wystąpień")
+
+    plt.plot(data1, color='r', label="Dane 1")
+    plt.plot(data2, color='b', label="Dane 2")
+    plt.legend()
+
+    plt.grid()
+
+    plt.xticks(range(len(names)), names, rotation=90)
+
+    plt.tight_layout()
+    plt.savefig('plot.png')
+    plt.show()
+
+def count_freq(fname):
+    with open(fname) as fp:
+        split_words = fp.read().split()
+        freq_dist = FreqDist(split_words)
+    return freq_dist
+
+def return_values(freq, words):
+    result = []
+    for s in words:
+        result.append(freq[s] if s in freq else 0)
+    return result
+
+
+def make_plot(file1, file2, words):
+    freq1 = count_freq(file1)
+    freq2 = count_freq(file2)
+
+    print(return_values(freq1, words))
+    print(return_values(freq2, words))
+
+    line_plot(return_values(freq1, words), return_values(freq2, words), words)
+
+make_plot(title1, title2, words)
+
+# teraz dopisz kod tworzący wykres punktowy zawierający liczbę wystąpień słów z listy
+# z punktami różnych kolorów dla pierwszego i drugiego tomu "Lalki"
+# dostosuj legendę itp.
+
+# podpowiedź: funkcję plt.plot(y) zastępuje funkcja plt.scatter(x, y)
+
